ee65d55b95e1701445cdb630050a3f628e0c3f8a
Axel Wettervik <axel.wettervik@gmail.com>
2021-10-31 15:50:01 +0900

    fix: json property names for read api key

--- WIMP-Server/Dtos/Users/ReadApiKeyDto.cs
+++ WIMP-Server/Dtos/Users/ReadApiKeyDto.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace WIMP_Server.Dtos.Users
 {
     public class ReadApiKeyDto
     {
+        [JsonPropertyName("key")]
         public string Key { get; set; }
+
+        [JsonPropertyName("roles")]
         public IEnumerable<string> Roles { get; set; }
     }
 }
